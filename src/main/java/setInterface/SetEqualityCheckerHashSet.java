a6dde427e5b588df384120eceaa369edf01cff22
Souvik Bera <souvikbera020@gmail.com>
2025-03-28 13:36:12 +0530

    [souvik]Added a method to compute the symmetric difference of two sets in Java.

---
+++ src/main/java/setInterface/SetEqualityCheckerHashSet.java
@@ -0,0 +1,2 @@
+package setInterface;public class SetEqualityCheckerHashSet {
+}
---
+++ src/main/java/setInterface/SetEqualityCheckerSymmetric.java
@@ -0,0 +1,40 @@
+package setInterface;
+
+import java.util.*;
+
+public class SetEqualityCheckerSymmetric {
+    public static boolean areSetsEqual(Set<Integer> set1, Set<Integer> set2) {
+        return set1.equals(set2);
+    }
+
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> unionSet = new HashSet<>(set1);
+        unionSet.addAll(set2);
+        return unionSet;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> intersectionSet = new HashSet<>(set1);
+        intersectionSet.retainAll(set2);
+        return intersectionSet;
+    }
+
+    public static Set<Integer> symmetricDifference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> symDiffSet = new HashSet<>(set1);
+        symDiffSet.addAll(set2);
+        Set<Integer> intersectionSet = intersection(set1, set2);
+        symDiffSet.removeAll(intersectionSet);
+        return symDiffSet;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+
+        System.out.println("Are sets equal? " + areSetsEqual(set1, set2));
+        System.out.println("Union: " + union(set1, set2));
+        System.out.println("Intersection: " + intersection(set1, set2));
+        System.out.println("Symmetric Difference: " + symmetricDifference(set1, set2));
+    }
+}
+
