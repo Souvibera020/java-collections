f18e9249e32197a43d673b6e2a0e2815d9f787fd
Souvik Bera <souvikbera020@gmail.com>
2025-03-28 13:38:14 +0530

    [souvik]Added a method to check if one set is a subset of another in Java.

---
+++ src/main/java/setInterface/SetEqualityCheckerSubsets.java
@@ -0,0 +1,54 @@
+package setInterface;
+
+import java.util.*;
+
+public class SetEqualityCheckerSubsets {
+    public static boolean areSetsEqual(Set<Integer> set1, Set<Integer> set2) {
+        return set1.equals(set2);
+    }
+
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> unionSet = new HashSet<>(set1);
+        unionSet.addAll(set2);
+        return unionSet;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> intersectionSet = new HashSet<>(set1);
+        intersectionSet.retainAll(set2);
+        return intersectionSet;
+    }
+
+    public static Set<Integer> symmetricDifference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> symDiffSet = new HashSet<>(set1);
+        symDiffSet.addAll(set2);
+        Set<Integer> intersectionSet = intersection(set1, set2);
+        symDiffSet.removeAll(intersectionSet);
+        return symDiffSet;
+    }
+
+    public static List<Integer> convertSetToSortedList(Set<Integer> set) {
+        List<Integer> sortedList = new ArrayList<>(set);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+
+    public static boolean isSubset(Set<Integer> subset, Set<Integer> superset) {
+        return superset.containsAll(subset);
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+        Set<Integer> set3 = new HashSet<>(Arrays.asList(5, 3, 9, 1));
+        Set<Integer> subset = new HashSet<>(Arrays.asList(2, 3));
+        Set<Integer> superset = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+
+        System.out.println("Are sets equal? " + areSetsEqual(set1, set2));
+        System.out.println("Union: " + union(set1, set2));
+        System.out.println("Intersection: " + intersection(set1, set2));
+        System.out.println("Symmetric Difference: " + symmetricDifference(set1, set2));
+        System.out.println("Sorted List from Set: " + convertSetToSortedList(set3));
+        System.out.println("Is subset a subset of superset? " + isSubset(subset, superset));
+    }
+}
